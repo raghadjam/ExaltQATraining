d2cc50cc25cbbd65c7595b7f39f8eb4172a687b7
Raghad Jamhour <Raghad.Jamhour@exalt.ps>
2025-08-21 11:11:36 +0300

    project without bugs

--- Assignment-8/Dental Booking System UI/tests/conftest.py
+++ Assignment-8/Dental Booking System UI/tests/conftest.py
@@ -1,14 +1,24 @@
 import time
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from pages.authentication_page.authentication_page import AuthPage
 from pages.main_page.main_page import MainPage
 from support import config
 from support import utils
 
+def create_headless_driver():
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    driver = webdriver.Chrome(options=chrome_options)  
+    return driver
+
 @pytest.fixture
 def setup_create_teardown_delete():
-    driver = webdriver.Chrome()
+    driver = create_headless_driver()
     page = AuthPage(driver)
     driver.get(config.BASE_URL)
     username = utils.create_user(page)
@@ -18,7 +28,7 @@ def setup_create_teardown_delete():
 
 @pytest.fixture
 def setup_get_driver():
-    driver = webdriver.Chrome()
+    driver = create_headless_driver()
     page = AuthPage(driver)
     driver.get(config.BASE_URL)
     yield page
@@ -27,11 +37,7 @@ def setup_get_driver():
 @pytest.fixture
 def setup_main_page(setup_create_teardown_delete):
     auth_page, username = setup_create_teardown_delete
-
     max_date = utils.getNextDate()
     auth_page.fill_login(username, config.VALID_PASSWORD)
-
     main_page = MainPage(auth_page.driver)  
-
     yield main_page, max_date
-
